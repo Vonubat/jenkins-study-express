eb6bba5b882c6a1ad659755f0fd982f3634c0ef2
Vonubat <vonubat@gmail.com>
2026-04-03 12:04:36 +0300

    Add basic tests

---
+++ tests/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Express App Dummy Tests', function () {
+  it('should pass this basic math test', function () {
+    assert.strictEqual(1 + 1, Math.random() > 0.1 ? 2 : 1); // Randomly fail to simulate a flaky test
+  });
+
+  it('should simulate checking the express server port', function () {
+    const port = 3000;
+    assert.strictEqual(port, 3000);
+  });
+});
